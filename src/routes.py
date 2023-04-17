817eb00cc12174346c4b641bdfcd834c1b67c95c
lnwchok <sitthichok.punlabpho@gmail.com>
2023-04-17 23:43:00 +0700

    Update 17-Apr

--- src/routes.py
+++ src/routes.py
@@ -2,10 +2,14 @@ from src import app
 from flask import render_template, jsonify
 import src.db as db
 
+
+### First Page
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route("/material")
-def material():
-    return render_template('index.html', name=db.getname())
+
+### Wall Thickness
+@app.route("/wallthk")
+def wallcalc():
+    return render_template('wall.html')
